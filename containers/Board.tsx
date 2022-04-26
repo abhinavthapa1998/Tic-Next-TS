f9ead4bac79cf0c647297e09d1dac702b5a58245
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-04-26 12:56:46 +0530

    commit

--- containers/Board.tsx
+++ containers/Board.tsx
@@ -1,26 +1,7 @@
 import { useEffect, useState } from "react";
 import Square from "../components/Square";
+import calculateWinner from "../logic/Calculate";
 type Player = "X" | "O" | "BOTH" | null;
-
-function calculateWinner(squares: Player[]) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
 
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
